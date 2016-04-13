567fa095857382b217df8eaa67354a99d9bb79af
tunamin <tunalamin88@gmail.com>
2016-04-13 13:38:23 +0800

    insert

--- client/components/client/form/clientform.js
+++ client/components/client/form/clientform.js
@@ -1,1 +1,51 @@
 Template.LookupAddClient.helpers({});
+
+Template.LookupAddClient.events({
+
+	'submit form' : function(e , tpl){
+
+		e.preventDefault();
+		var form = $(e.currentTarget);
+
+
+		swal({
+			title: '',
+			text: 'Adakah anda pasti untuk menyimpan maklumat ini?',
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonText: "YA",
+			cancelButtonText: "TIDAK",
+			closeOnConfirm: false
+		}), function(isConfirm){
+
+			if(isConfirm){
+
+				var data = typeof FormData(form)
+
+				_.extend(data, {
+					status : 'aktif',
+					process:1
+				});
+
+				Meteor.call ('claim.save' , data, function(err){
+
+					if(err){
+						swal ('' , err.message, 'error');
+					}
+					else {
+						swal ({
+							title: '',
+							text: 'Maklumat telah berjaya disimpan.',
+							confirmButtonText: 'OK',
+							type: 'success'
+						})
+
+						tpl.find('form').reset();
+						$(tpl.firstNode).modal('hide');
+					}
+				})
+			}
+		}
+	}
+
+})
--- client/router/client.js
+++ client/router/client.js
@@ -2,8 +2,3 @@ Router.route('/client', {
 	name: 'client',
 	controller: 'ClientCtrl'
 })
-
-Router.route('/client/form', {
-	name: 'client.form',
-	controller: 'ClientformCtrl'
-})
